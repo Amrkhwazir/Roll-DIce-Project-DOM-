8c8d064fea70fcd88e0897649277649663771d1e
Amirkhan567 <amrkh5678@gmail.com>
2023-03-14 13:21:11 +0500

    dice roll complete

--- app.js
+++ app.js
@@ -22,40 +22,72 @@ console.log(score1)
 var score2 = document.querySelector('.score2')
 console.log(score2)
 
-var col = document.querySelector('.col')
-console.log(col)
-
+var player1 = document.querySelector('.player1')
+var player2 = document.querySelector('.player2')
 
 
 rollBtn.addEventListener('click',rollDiceHandler)
 
-function rollDiceHandler(){
-
-    // Random number generate //
-
-    var diceNumber = Math.round(Math.random()*5)+1
-    console.log(diceNumber)
-    // hidden class remove //
-
-    diceImg.classList.remove('hidden')
-    diceImg.src = `./Assets/${diceNumber}.png`
-
-    if(diceNumber == 1){
-        score1.textContent = 0  
+    var currentPlayer = 1
+    function rollDiceHandler(){
+        
+        // Random number generate //
+    
+        var diceNumber = Math.round(Math.random()*5)+1
+        console.log(diceNumber)
+        // hidden class remove //
+    
+        diceImg.classList.remove('hidden')
+        diceImg.src = `./Assets/${diceNumber}.png`
+    
+        if (diceNumber == 1) {
+            if(currentPlayer == 1){
+    
+                score1.textContent = 0
+                currentPlayer = 2
+            }else{
+                currentPlayer = 1
+                score2.textContent = 0
+            }
+            player1.classList.toggle('active')
+            player2.classList.toggle('active')
+    
+    }else{
+        if(currentPlayer == 1){
+            score1.textContent = Number(score1.textContent) + diceNumber
+            totalPlayerScore1.textContent = Number(totalPlayerScore1.textContent) + Number(score1.textContent)
+    
+        }else{
+            score2.textContent = Number(score2.textContent) + diceNumber
+            totalPlayerScore2.textContent = Number(totalPlayerScore2.textContent) + Number(score2.textContent)
+        
+        }
     }
-     else if(diceNumber != 0){
-        score1.textContent = Number(diceNumber)
-        totalPlayerScore1.textContent = Number(totalPlayerScore1.textContent) + Number(score1.textContent)
-    }}
+    }
+    
+        // hold button //
+        holdBtn.addEventListener('click',holdHandler)
+        function holdHandler(){
+    
+            player1.classList.toggle('active')
+            player2.classList.toggle('active')
 
-    // hold button //
-    holdBtn.addEventListener('click',holdHandler)
-    function holdHandler(){
-}
-    // Reload Game //
-
-newBtn.addEventListener('click', reloadGame)
-function reloadGame(){
-    window.location.reload()
-}
-
+            if(totalPlayerScore1.textContent > totalPlayerScore2.textContent || totalPlayerScore1 == 100){
+            alert("Congratulation Player1 Win's with Highest Marks ")
+        }else if(totalPlayerScore1.textContent < totalPlayerScore2.textContent || totalPlayerScore2 == 100) {
+            alert("Congratulation Player2 Win's with Highest Marks")
+            
+        }else{
+            alert("Both have Same Score")
+    
+        }
+    
+    }
+        // Reload Game //
+    
+    newBtn.addEventListener('click', reloadGame)
+    function reloadGame(){
+        window.location.reload()
+    }
+    
+    
